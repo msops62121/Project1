a6a3fe7268e29a38dfac045f357db8edbe3d3b29
mohan <mohan14242@gmail.com>
2024-12-25 15:16:35 +0530

    push the code

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="Mohan",
+    author_email="mops@gmail.com",
+    packages=find_packages()
+)
