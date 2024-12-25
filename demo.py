6e831773ca909aece934b07171696e60da1cdca6
mohan <mohan14242@gmail.com>
2024-12-25 21:58:53 +0530

    push the code

--- demo.py
+++ demo.py
@@ -1,8 +1,4 @@
 from us_visa.logger import logging 
 from us_visa.exception import USvisaException
 import sys
-logging.info("i am learning the MLOPs")
-try: 
-    a=2/0 
-except Exception as e: 
-    raise USvisaException(e,sys)
+logging.info("just testing the package")
